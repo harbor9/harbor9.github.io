ba2c9b0d35755c6f25af5323202c01a143bf3c0a
sleski <slawomir.leski@gmail.com>
2019-11-02 21:09:58 +0100

    Added i18n. ;-)

--- messages.js
+++ messages.js
@@ -1,13 +1,33 @@
 const plConfig = {
-    welcome: "ponizej",
-    hi: "powyzej"
+    welcome: "Witamy",
+    title: "To jest strona Apartamentu Harbor9!",
+    goodbye: "Zegnamy"
 }
+
+const enConfig = {
+    welcome: "Welcome",
+    title: "This is the website of the Harbor9 Apartment!",
+    goodbye: "Goodbye"
+}
+
+const deConfig = {
+    welcome: "Willkommen",
+    title: "Dies ist die Website der Ferienwohnung Harbor9!",
+    goodbye: "Auf Wiedersehen "
+}
+
+var browserLanguage = (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.language;
 
 const replaceText = (el) => {
     const key = el.innerText;
-    el.innerText = plConfig[key] || key;
+    if (browserLanguage.toLowerCase().startsWith("pl")) {
+        el.innerText = plConfig[key] || key;
+    } else if (browserLanguage.toLowerCase().startsWith("de")) {
+        el.innerText = deConfig[key] || key;
+    } else {
+        el.innerText = enConfig[key] || key;
+    }
 }
 
 const elements = document.querySelectorAll("[data-i18n]");
-console.log(elements);
 elements.forEach(el => replaceText(el));
