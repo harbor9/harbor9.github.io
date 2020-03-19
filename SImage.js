457de39f7ec60059873c9609589af7f91546e73c
sleski <slawomir.leski@gmail.com>
2020-03-19 22:32:58 +0100

    Changed gallery

--- SImage.js
+++ SImage.js
@@ -3,10 +3,11 @@ export default class SImage extends HTMLElement {
       super();
       this.photoId = this.getAttribute("photoId") || "sid";
       this.class = this.getAttribute("class") || "big";
+      this.previewSize = this.getAttribute("previewSize") || "w";
     }
 
     connectedCallback() {
-      this.innerHTML = `<div class="${this.class}"><img id="myImg" src="${this.photoId}w.jpg" onclick="setImage('${this.photoId}','c.jpg')"> </div>`;
+      this.innerHTML = `<div class="${this.class}"><img id="myImg" src="${this.photoId}${this.previewSize}.jpg" onclick="setImage('${this.photoId}','c.jpg')"> </div>`;
     }
   }
 
