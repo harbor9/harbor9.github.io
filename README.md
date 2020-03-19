d717574a1f4091abc386b93f781d59baa63ec4ab
sleski <slawomir.leski@gmail.com>
2020-03-19 21:41:36 +0100

    Updated readme + edited gallery

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Harbor9 Appartment Page https://harbor9.github.io/
   * google maps
   * form/contact
   * galerry
+
+# How page was created
+
+* Big thank to @gurukulkarni who shared his knowledge, experience and time
+* This tutorials were very helpful:
+  * https://www.webcomponents.org/
+  * https://scrimba.com/p/pvwkvhx/czk8deu9
+  * https://www.w3schools.com/css/tryit.asp?filename=trycss_image_modal_js
--- SImage.js
+++ SImage.js
@@ -2,10 +2,11 @@ export default class SImage extends HTMLElement {
     constructor() {
       super();
       this.photoId = this.getAttribute("photoId") || "sid";
+      this.class = this.getAttribute("class") || "big";
     }
 
     connectedCallback() {
-      this.innerHTML = `<img id="myImg" src="${this.photoId}w.jpg" onclick="setImage('${this.photoId}','c.jpg')">`;
+      this.innerHTML = `<div class="${this.class}"><img id="myImg" src="${this.photoId}w.jpg" onclick="setImage('${this.photoId}','c.jpg')"> </div>`;
     }
   }
 
