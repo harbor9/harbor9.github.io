4e4aafb364d49257143b8303de73c016dbf2a96a
sleski <slawomir.leski@gmail.com>
2020-03-19 15:13:04 +0100

    Work in progress....

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+function setImage(src = "") {
+    console.log("Hello world! " + src);
+
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("imgPreview");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = "https://live.staticflickr.com/1889/42676514890_a460f56471_c.jpg"
+    captionText.innerHTML = img.alt;
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
