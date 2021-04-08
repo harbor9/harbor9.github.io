e3b7d52bb65706e58ae1ad58a61fc81b12d12561
MLeska <46657414+MLeska@users.noreply.github.com>
2021-04-08 08:01:27 +0200

    Update messages.js (#13)

--- messages.js
+++ messages.js
@@ -1,18 +1,18 @@
 const plConfig = {
-    welcome: "Witamy",
     title: "Apartament Harbor 9 w Kołobrzegu",
-    goodbye: "Żegnamy"
+    welcome: "Serdecznie Państwa zapraszamy do spędzenia urlopu w naszym apartamencie. Apartament Harbor 9 jest przestronnym, nowoczesnym mieszkaniem z ogromnym, narożnym balkonem oraz miejscem postojowym w podziemnym garażu. Stanowi wygodne miejsce do wypoczynku i relaksu, z możliwością dostawienia łóżeczka turystycznego dla małego dziecka (od 0 do 3 lat). Usytuowany jest niedaleko plaży zachodniej i pięknego tarasu widokowego, gdzie można podziwiać niezapomniane zachody słońca.",
+    goodbye: "Zapraszamy do rezerwacji telefonicznej lub za pomocą formularza strony, bądź za pomocą stron booking.com lub airbnb"
 }
 
 const enConfig = {
-    welcome: "Welcome",
     title: "Harbor 9 Apartment in Colberg",
+    welcome: "Dear Guests, we would like to invite you to spend your holiday in our apartment, which has been designed and equipped with guest comfort in mind. Moreover it has a huge corner balcony and a parking space in the underground garage. The location is close to the west beach and the beautiful observation deck, where you can enjoy unforgettable sunsets. A holiday in Harbor 9 Apartment provides you with pleasant stay and plenty of opportunities for seaside attractions.",
     goodbye: "Goodbye"
 }
 
 const deConfig = {
-    welcome: "Herzlich Willkommen",
     title: "Harbor 9 Apartment in Colberg",
+    welcome: "Liebe Gäste, wir möchten Sie einladen, Ihren Urlaub in unserer Wohnung zu verbringen, die auf Gästekomfort ausgelegt und ausgestattet ist. Unser Anwesen liegt in der Nähe des Weststrandes und der wunderschönen Aussichtsplattform, auf der Sie unvergessliche Sonnenuntergänge genießen können. Ein Urlaub im Harbor 9 Apartment bietet Ihnen einen angenehmen Aufenthalt und viele Möglichkeiten für Attraktionen am Meer.",
     goodbye: "Auf Wiedersehen "
 }
 
