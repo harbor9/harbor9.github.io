190c2c54296558c5ef0d19dd72ca1f7c357587b6
sleski <slawomir.leski@gmail.com>
2023-02-28 23:11:18 +0100

    Added possibility to switch back (till 0)

--- SImage.js
+++ SImage.js
@@ -4,10 +4,11 @@ export default class SImage extends HTMLElement {
       this.photoId = this.getAttribute("photoId") || "sid";
       this.class = this.getAttribute("class") || "big";
       this.previewSize = this.getAttribute("previewSize") || "n";
+      this.indexImage = this.getAttribute("indexImage") || "1"
     }
 
     connectedCallback() {
-      this.innerHTML = `<div class="${this.class}"><img id="myImg" src="${this.photoId}${this.previewSize}.jpg" onclick="setImage('${this.photoId}','c.jpg')"> </div>`;
+      this.innerHTML = `<div class="${this.class}"><img id="myImg" src="${this.photoId}${this.previewSize}.jpg"               onclick="setImage('${this.indexImage}','${this.photoId}','c.jpg')" indexImage="${this.indexImage}"> </div>`;
     }
   }
 
--- app.js
+++ app.js
@@ -11,9 +11,11 @@ navLinks.forEach(link => {
     })
 })
 
-function setImage(src = "", sufix = "w.jpg") {
+function setImage(index, src = "", sufix = "w.jpg") {
 
     var modal = document.getElementById('myModal');
+    // index we need to set to be able to switch previous and next image
+    document.getElementById('currentIndex').setAttribute('value', index)
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img = document.getElementById('myImg');
@@ -24,21 +26,28 @@ function setImage(src = "", sufix = "w.jpg") {
     captionText.innerHTML = img.alt;
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var closeSpan = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-        document.getElementById("imgPreview").src = '';
+    closeSpan.onclick = function() {
+      modal.style.display = "none";
+      document.getElementById("imgPreview").src = '';
+      document.getElementById('currentIndex').setAttribute('value', '')
     }
 
     var spanPreviousImg = document.getElementsByClassName("previousImg")[0];
 
     spanPreviousImg.onclick = function() {
-      console.log('PreviousImg clicked!');
-      setImage('https://live.staticflickr.com/1872/29548347767_27d80564ce_','c.jpg')
+      var currentIndex = document.getElementById('currentIndex').getAttribute('value')
+      const sImages = document.getElementsByTagName("s-image")
+      var sImage = sImages[currentIndex-1]
+      setImage(sImage.indexImage, sImage.photoId, 'c.jpg')
     }
 
+    var spanNextImg = document.getElementsByClassName("nextImg")[0];
+    spanNextImg.onclick = function() {
+      setImage('https://live.staticflickr.com/1872/29548347767_27d80564ce_','c.jpg')
+    }
     // Get the <span> element that show the image on the right (previous)
 
     // Get the <span> element that show the image on the left (next)
